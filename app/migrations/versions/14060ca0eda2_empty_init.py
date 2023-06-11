1f2d96d046dc9beb01c5196905a638f38c4ff504
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-06-11 22:33:27 +0300

    Completed assignment

---
+++ app/migrations/versions/14060ca0eda2_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 14060ca0eda2
+Revises: 
+Create Date: 2023-06-11 22:24:27.274780
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '14060ca0eda2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
